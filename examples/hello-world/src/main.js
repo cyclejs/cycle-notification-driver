cd4badea9d7b1bca730a21f561f4a7746ac258b8
Brandon Smith <bsmith@bandwidth.com>
2015-08-27 13:07:20 -0400

    A simple example

---
+++ examples/hello-world/src/main.js
@@ -0,0 +1,35 @@
+import Cycle from '@cycle/core'
+import { makeNotificationDriver } from 'cycle-notification-driver'
+
+let { Rx } = Cycle
+
+function main({notification}) {
+
+    let notifications$ = Rx.Observable
+            .interval(10000)
+            .startWith(-1)
+            .map( (value) => ({
+                title: 'Test Notification',
+                body: `Interval ${value}`,
+                tag: 'test-notification',
+                icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAIAAAACDbGyAAAAEUlEQVQIW2Pg3uSLjBgo5AMACSoZ+1zqJ8AAAAAASUVORK5CYII='
+            }) )
+            .take(10),
+        show$ = notification.get('show'),
+        click$ = notification.get('click'),
+        error$ = notification.get('error'),
+        close$ = notification.get('close'),
+        all$ = Rx.Observable.merge(show$, click$, error$, close$)
+
+    all$.do(args => console.log(args))
+
+    return {
+        notification: notifications$
+    }
+}
+
+const drivers = {
+  notification: makeNotificationDriver()
+}
+
+Cycle.run(main, drivers)
